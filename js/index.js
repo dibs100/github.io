c016cebf293b3fcf31904ce594e3d2c651ad415b
dibs100 <dibesh.shresstha@gmail.com>
2026-02-01 15:00:02 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -277,7 +277,7 @@ function initializeZabbixButton() {
         zabbixBtn.addEventListener('click', () => {
             console.log("🌐 Redirecting to Zabbix website...");
             // Open in a new tab
-            window.open('https://www.zabbix.com', '_blank');
+            window.open('https://zabbix-web.5fpbvwltlg7opaq3.myfritz.net', '_blank');
         });
     } else {
         console.error("❌ Zabbix button not found!");
